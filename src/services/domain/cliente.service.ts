bdfde45e8e676397d45a14da310ab1b3e9ab631d
uniom3 <48806717+uniom3@users.noreply.github.com>
2020-08-29 18:46:03 -0300

    Mostrando dados e imagem do cliente para página de profile

---
+++ src/services/domain/cliente.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs/Rx";
+import { ClienteDTO } from "../../models/cliente.dto";
+import { API_CONFIG } from "../../config/api.config";
+import { StorageService } from "../storage.service";
+
+@Injectable()
+export class ClienteService {
+    constructor(public http: HttpClient, public storage: StorageService) {
+    }
+
+    findByEmail(email: string): Observable<ClienteDTO> {
+
+        let token = this.storage.getLocalUser().token;
+        let autHeader = new HttpHeaders({'Authorization': 'Bearer '+token});
+
+        return this.http.get<ClienteDTO>(
+            `${API_CONFIG.baseUrl}/clientes/email?value=${email}`,
+            {'headers': autHeader});
+    }
+
+    getImageFromBucket(id: string) : Observable<any> {
+        let url = `${API_CONFIG.bucketBaserUrl}/cp${id}.jpg`
+        return this.http.get(url, {responseType: 'blob'});
+    }
+
+}
